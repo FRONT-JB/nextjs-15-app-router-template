df44c7622b27c51e11fc8228b44a43419f793e44
전병길 <jeonbg@kakao.com>
2025-02-15 02:53:24 +0900

    🚚 chore: Nextjs Template 추가

---
+++ README.md
@@ -0,0 +1,35 @@
+# NextJS 15 App Router Template
+
+## Getting Started
+
+### Using this template
+
+You can use this template in two ways:
+
+1. **Use as template**
+   - Click the "Use this template" button on the GitHub repository
+   - Select "Create a new repository"
+   - Choose your repository name and settings
+   - Click "Create repository from template"
+
+2. **Clone directly**
+>    Clone the repository
+
+   ```bash
+   git clone https://github.com/front-jb/nextjs-15-app-router-template.git
+   ```
+
+>   Move to the project directory
+
+   ```bash
+   cd nextjs-15-app-router-template
+   ```
+
+>   Install dependencies
+
+   ```bash
+   npm install
+   
+   # Run the development server
+   npm run dev
+   ```
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import { Geist_Mono, Noto_Sans } from 'next/font/google';
+import './globals.css';
+
+const notoSans = Noto_Sans({
+  variable: '--font-noto-sans',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Nextjs 15 App Router Template',
+  description: 'Nextjs 15 App Router Template',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`${notoSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
